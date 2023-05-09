2aa43d80d5bb57a7280f6fac22a1b33fab9a78dd
Chris Lexmond <chris@chrislexmond.com>
2023-05-08 20:19:06 -0400

    Added G constant

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import elements from './elements.js';
 import lambert from './lambert.js';
 
 export { angles, elements, lambert };
+
+export const GRAVITATIONAL_CONSTANT = 6.67430E-11; // m3 kg-1 s-2
